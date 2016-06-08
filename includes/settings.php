e567690b65579e0f8fb8c93ea12f849c5fda51e0
Vitaliy Senenko <senenkovitalik@gmail.com>
2016-06-08 22:43:17 +0300

    feat: User can choose pages which will show bar with spec user_id

--- includes/settings.php
+++ includes/settings.php
@@ -21,11 +21,46 @@
 <form id="form_db_set">
     <table>
         <tr>
-            <td colspan="2">If you don't have account yet, please <a href="https://rabbut.com/">visit</a> out site.</td>
+            <td colspan="2">If you don't have account yet, please <a href="https://rabbut.com/">visit</a> our web-site.</td>
         </tr>
         <tr>
             <td>User ID</td>
             <td><input type="text" id="user_id" title="User ID" value="<?php echo $s['user_id']; ?>"  size="40" required></td>
+        </tr>
+        <tr>
+            <td>use for</td>
+            <td>
+            <?php
+                $args = array(
+                    'sort_order'   => 'ASC',
+                    'sort_column'  => 'post_title',
+                    'hierarchical' => 1,
+                    'exclude'      => '',
+                    'include'      => '',
+                    'meta_key'     => '',
+                    'meta_value'   => '',
+                    'authors'      => '',
+                    'child_of'     => 0,
+                    'parent'       => -1,
+                    'exclude_tree' => '',
+                    'number'       => '',
+                    'offset'       => 0,
+                    'post_type'    => 'page',
+                    'post_status'  => 'publish',
+                ); 
+                // get array of WP_post's
+                $pages = get_pages( $args );
+            ?>
+            <!-- Add dropdown list of pages -->
+            <select name="pages">
+                <option value="front_page">Front page</option>
+                <?php
+                    foreach($pages as $p) {
+                        echo "<option value='$p->post_name'>$p->post_title</option>";
+                    }
+                ?>
+            </select>  
+            </td>
         </tr>
         <tr>
             <td colspan="2">
