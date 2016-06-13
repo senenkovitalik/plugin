af99cef79a8c388c29d91cc19b1466265487377a
Senenko Vitaliy <senenkovitalik@gmail.com>
2016-06-13 23:59:17 +0300

    fix: Place buttons into <div> and move him 5 px left.

--- includes/settings.php
+++ includes/settings.php
@@ -47,9 +47,12 @@ If you don't have account yet, please <a href="https://rabbut.com/">visit</a> ou
         ?>
     </table>
 
-    <input id="add_account" type="button" value="Add User ID" class="button button-primary">
-    <input id="submit_btn" type="submit" value="Submit" class="button button-primary">
-    <span id="stat" style="vertical-align: middle; margin-left: 20px;"></span>
+    <div style="position: relative; left: 5px;">
+        <input id="add_account" type="button" value="Add User ID" class="button button-primary" ">
+        <input id="submit_btn" type="submit" value="Submit" class="button button-primary">
+        <span id="stat" style="vertical-align: middle; margin-left: 20px;"></span>
+    </div>
+    
 </form>
 
 <script type="text/javascript">
