2c63efc60c5c4518a73c73ce4fc95ade0eca1db9
Senenko Vitaliy <senenkovitalik@gmail.com>
2016-06-14 15:51:08 +0300

    fix: Generating URL address.

--- subscription_bar.php
+++ subscription_bar.php
@@ -120,7 +120,7 @@ class SubscriptionBar {
 			script.id = \"rabbut-o-matic\";
 			script.type = \"text/javascript\";
 
-			var code = "."'(function() {function async_load(){ var s = document.createElement(\"script\"); s.type = \"text/javascript\"; s.async = true; var theUrl = \"https://devvy.rabbut.com/api/v1/js/$data_widget_id/\"; s.src = theUrl + ( theUrl.indexOf(\"?\") >= 0 ? \"&\" : \"?\") + \"ref=\" + encodeURIComponent(window.location.href);	var embedder = document.getElementById(\"rabbut-o-matic\");	embedder.parentNode.insertBefore(s, embedder); } if (window.attachEvent) { window.attachEvent(\"onload\", async_load); } else {	window.addEventListener(\"load\", async_load, false);}})();'"."
+			var code = "."'(function() {function async_load(){ var s = document.createElement(\"script\"); s.type = \"text/javascript\"; s.async = true; var theUrl = \"https://devvy.rabbut.com/api/v1/js/$data_widget_id\"; s.src = theUrl + ( theUrl.indexOf(\"?\") >= 0 ? \"&\" : \"?\") + \"ref=\" + encodeURIComponent(window.location.href);	var embedder = document.getElementById(\"rabbut-o-matic\");	embedder.parentNode.insertBefore(s, embedder); } if (window.attachEvent) { window.attachEvent(\"onload\", async_load); } else {	window.addEventListener(\"load\", async_load, false);}})();'"."
 
 			// create text node with text of new script and add it to new prev script
 			script.appendChild( document.createTextNode(code) );
