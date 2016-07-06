0c213766002630e9fd7c037f4b0c3dde50a83235
Vitaliy Senenko <senenkovitalik@gmail.com>
2016-07-06 23:41:58 +0300

    refactor: Create popup_div more pretty.

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,8 +1,15 @@
 jQuery(document).ready( function ($) {
   var wrap_div = $("div.wrap");
 
-  var popup_div = $("<div style=\"padding-right: 38px; position: relative;\"><h3>Popup</h3><p>Congratulations. You have installed plugin.</p></div>");
-  popup_div.addClass("updated notice "); //is-dismissible
+  var popup_div = $('<div />', {
+  	'class' : "updated notice",
+  	css : {
+  		paddingRight : "38px",
+  		position : "relative"
+  	},
+  	// You can change content of popup #1 by modifying line below
+  	html : '<h3>Popup</h3><p>Congratulations. You have installed plugin.</p>'
+  });
 
   var button = $('<button />', {
   	'class' : "notice-dismiss",
@@ -12,7 +19,7 @@ jQuery(document).ready( function ($) {
 		click : function() {
 			// close popup
 			popup_div.remove();
-			
+
 			// AJAX request
 			$.post(admin_object.ajax_url, {'action': 'popup_action', 'close': true});
 		}
